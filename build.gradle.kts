3cb9cebeb64c05f30aad4d6e104048d5df4c25aa
André Diogo <redan.blue27@gmail.com>
2018-02-20 22:26:48 +0000

    Add gitignore and simple builds in sub-project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     var kotlin_version: String by extra
+    var catalyst_version: String by extra
     kotlin_version = "1.2.21"
+    catalyst_version = "1.2.1"
 
     repositories {
         mavenCentral()
@@ -20,6 +22,7 @@ apply {
 }
 
 val kotlin_version: String by extra
+val catalyst_version: String by extra
 
 repositories {
     mavenCentral()
@@ -27,6 +30,14 @@ repositories {
 
 dependencies {
     compile(kotlinModule("stdlib-jdk8", kotlin_version))
+    compile("io.atomix.catalyst", "catalyst-buffer", catalyst_version)
+    compile("io.atomix.catalyst", "catalyst-common", catalyst_version)
+    compile("io.atomix.catalyst", "catalyst-concurrent", catalyst_version)
+    compile("io.atomix.catalyst", "catalyst-local", catalyst_version)
+    compile("io.atomix.catalyst", "catalyst-netty", catalyst_version)
+    compile("io.atomix.catalyst", "catalyst-serializer", catalyst_version)
+    compile("io.atomix.catalyst", "catalyst-transport", catalyst_version)
+    compile("io.atomix.catalyst", "catalyst-parent", catalyst_version)
 }
 
 tasks.withType<KotlinCompile> {
