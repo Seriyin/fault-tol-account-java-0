d26ab0ac115bb5df0a42ee4a5ea3ec7b4312d95d
André Diogo <redan.blue27@gmail.com>
2018-02-26 12:02:42 +0000

    Implement catalyst serializable.

--- lab0-mes/src/main/kotlin/pt/um/tf/lab0/lab0mes/Messaging.kt
+++ lab0-mes/src/main/kotlin/pt/um/tf/lab0/lab0mes/Messaging.kt
@@ -1,5 +1,30 @@
 package pt.um.tf.lab0.lab0mes
 
-data class Message(val op : Int, val mov : Int)
+import io.atomix.catalyst.buffer.BufferInput
+import io.atomix.catalyst.buffer.BufferOutput
+import io.atomix.catalyst.serializer.CatalystSerializable
+import io.atomix.catalyst.serializer.Serializer
 
-data class Reply(val op : Int, val res : Boolean, val balance : Int)
+data class Message(var op : Int, var mov : Int) : CatalystSerializable  {
+    override fun writeObject(buffer: BufferOutput<*>?, serializer: Serializer?) {
+        buffer?.writeInt(op)?.writeInt(mov);
+    }
+
+    override fun readObject(buffer: BufferInput<*>?, serializer: Serializer?) {
+        op = buffer!!.readInt();
+        mov = buffer.readInt();
+    }
+}
+
+data class Reply(var op : Int, var res : Boolean, var balance : Int) : CatalystSerializable {
+    override fun readObject(buffer: BufferInput<*>?, serializer: Serializer?) {
+        op = buffer!!.readInt()
+        res = buffer.readBoolean()
+        balance = buffer.readInt()
+    }
+
+    override fun writeObject(buffer: BufferOutput<*>?, serializer: Serializer?) {
+        buffer?.writeInt(op)?.writeBoolean(res)?.writeInt(balance)
+    }
+
+}
