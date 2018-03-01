7d5062aab167e76408e35732f00ef354bcfece8b
André Diogo <redan.blue27@gmail.com>
2018-03-01 00:08:52 +0000

    Keep vertx as an option.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     var kotlin_version: String = "1.2.21"
+    //  var vertx_version: String = "3.5.1"
     var catalyst_version: String = "1.2.1"
 
     repositories {
         mavenCentral()
+        jcenter()
     }
     dependencies {
         classpath(kotlinModule("gradle-plugin", kotlin_version))
@@ -23,6 +25,7 @@ subprojects {
     }
 
     var kotlin_version: String = "1.2.21"
+    // var vertx_version: String = "3.5.1"
     var catalyst_version: String = "1.2.1"
 
     repositories {
@@ -31,6 +34,12 @@ subprojects {
 
     dependencies {
         compile(kotlinModule("stdlib-jdk8", kotlin_version))
+/*
+        compile("io.vertx", "vertx-lang-kotlin", vertx_version)
+        compile("io.vertx", "vertx-lang-kotlin-coroutines", vertx_version)
+        compile("io.vertx", "vertx-core", vertx_version)
+*/
+        compile("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "0.22.3")
         compile("io.atomix.catalyst", "catalyst-buffer", catalyst_version)
         compile("io.atomix.catalyst", "catalyst-concurrent", catalyst_version)
         compile("io.atomix.catalyst", "catalyst-local", catalyst_version)
