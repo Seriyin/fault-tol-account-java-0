61cb9910c343302aaff7179482bf29dc92951c49
André Diogo <redan.blue27@gmail.com>
2018-06-26 20:27:02 +0100

    Separates messaging into its own classes.

--- lab0-mes/src/main/kotlin/pt/um/tf/lab0/lab0mes/Reply.kt
+++ lab0-mes/src/main/kotlin/pt/um/tf/lab0/lab0mes/Reply.kt
@@ -5,17 +5,6 @@ import io.atomix.catalyst.buffer.BufferOutput
 import io.atomix.catalyst.serializer.CatalystSerializable
 import io.atomix.catalyst.serializer.Serializer
 
-data class Message(var op : Int = 0, var mov : Int = 0) : CatalystSerializable  {
-    override fun writeObject(buffer: BufferOutput<*>?, serializer: Serializer?) {
-        buffer?.writeInt(op)?.writeInt(mov);
-    }
-
-    override fun readObject(buffer: BufferInput<*>?, serializer: Serializer?) {
-        op = buffer!!.readInt();
-        mov = buffer.readInt();
-    }
-}
-
 data class Reply(var op : Int = 0, var denied : Boolean = false, var balance : Int = 0) : CatalystSerializable {
 
     override fun readObject(buffer: BufferInput<*>?, serializer: Serializer?) {
