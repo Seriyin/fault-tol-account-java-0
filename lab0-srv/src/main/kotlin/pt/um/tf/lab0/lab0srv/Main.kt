b421d189d994077ded60c74f74a1efb185a48231
André Diogo <redan.blue27@gmail.com>
2018-06-26 20:27:41 +0100

    Adds logging + fixes serialization + abridges replying.

--- lab0-srv/src/main/kotlin/pt/um/tf/lab0/lab0srv/Main.kt
+++ lab0-srv/src/main/kotlin/pt/um/tf/lab0/lab0srv/Main.kt
@@ -6,6 +6,7 @@ import io.atomix.catalyst.serializer.Serializer
 import io.atomix.catalyst.transport.Address
 import io.atomix.catalyst.transport.Connection
 import io.atomix.catalyst.transport.netty.NettyTransport
+import mu.KLogging
 import pt.um.tf.lab0.lab0mes.Message
 import pt.um.tf.lab0.lab0mes.Reply
 import java.util.concurrent.CompletableFuture
@@ -16,34 +17,38 @@ fun main(args : Array<String>) {
     main.run()
 }
 
-class Main {
-    val me = Address("127.0.0.1", 22556)
-    val t = NettyTransport()
-    val sr = Serializer()
-    val tc = SingleThreadContext("srv-%d",sr)
-    val acc = Account()
+private class Main {
+    companion object : KLogging()
+    private val me = Address("127.0.0.1", 22556)
+    private val t = NettyTransport()
+    private val sr = Serializer()
+    private val tc = SingleThreadContext("srv-%d",sr)
+    private val acc = Account()
 
-    fun run() : Unit {
-        tc.execute({t.server().listen(me, this::handlers)}).join()
+    fun run() {
+        sr.register(Message::class.java)
+        sr.register(Reply::class.java)
+        tc.execute {
+            t.server().listen(me, this::handlers)
+        }.join()
         while(readLine() == null);
         tc.close()
         t.close()
         println("I'm here")
     }
 
-    fun handlers(connection: Connection) {
-        println("Handling connection")
+    private fun handlers(connection: Connection) {
+        logger.info("Handling connection")
         connection.handler(Message::class.java, this::handleOp)
     }
 
-    fun handleOp(it : Message) : CompletableFuture<Reply> {
-        val res : CompletableFuture<Reply>
-        when (it.op) {
-            0 -> res = Futures.completedFuture(Reply(it.op, true, acc.balance()))
-            1 -> res = Futures.completedFuture((Reply(it.op, !acc.movement(it.mov), 0)))
-            else -> res = Futures.exceptionalFuture(IllegalArgumentException("Not recognized Message"))
+    private fun handleOp(it : Message) : CompletableFuture<Reply> {
+        logger.info { "Handle -> ${it.mov}" }
+        return when (it.op) {
+            0 -> Futures.completedFuture(Reply(it.op, true, acc.balance()))
+            1 -> Futures.completedFuture((Reply(it.op, !acc.movement(it.mov), it.mov)))
+            else -> Futures.exceptionalFuture(IllegalArgumentException("Not recognized Message"))
         }
-        return res
     }
 }
 
