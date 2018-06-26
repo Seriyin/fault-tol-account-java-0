22aa18cacd38faecde6cbb54dae7ab370c0b1df9
André Diogo <redan.blue27@gmail.com>
2018-06-26 20:25:48 +0100

    Adds logging + private variables used.

--- lab0-cli/src/main/kotlin/pt/um/tf/lab0/lab0cli/BankStub.kt
+++ lab0-cli/src/main/kotlin/pt/um/tf/lab0/lab0cli/BankStub.kt
@@ -1,16 +1,18 @@
 package pt.um.tf.lab0.lab0cli
 
 import io.atomix.catalyst.concurrent.ThreadContext
-import io.atomix.catalyst.serializer.Serializer
 import io.atomix.catalyst.transport.Address
 import io.atomix.catalyst.transport.Connection
 import io.atomix.catalyst.transport.Transport
+import mu.KLogging
 import pt.um.tf.lab0.lab0mes.Message
 import pt.um.tf.lab0.lab0mes.Reply
 import pt.um.tf.lab0.lab0mes.Bank
 import java.util.concurrent.CompletableFuture
 
-class BankStub(val me: Address, val t: Transport, val sr: Serializer, val tc: ThreadContext) : Bank {
+class BankStub(private val me: Address, private val t: Transport, private val tc: ThreadContext) : Bank {
+    companion object : KLogging()
+
 
     private var conF : CompletableFuture<Connection> = CompletableFuture()
 
@@ -27,6 +29,7 @@ class BankStub(val me: Address, val t: Transport, val sr: Serializer, val tc: Th
             conF.thenApply {
                 it.sendAndReceive<Message, Reply>(Message(1, mov)).thenAccept {
                     res.complete(it.denied)
+                    logger.info("Move denial : ${it.denied}, ${it.balance}")
                 }
             }
         }
@@ -39,7 +42,7 @@ class BankStub(val me: Address, val t: Transport, val sr: Serializer, val tc: Th
             conF.thenApply {
                 it.sendAndReceive<Message, Reply>(Message()).thenAccept {
                     res.complete(it.balance)
-                    println("Balance incoming ${it.balance}")
+                    logger.info("Balance incoming ${it.balance}")
                 }
             }
         }
